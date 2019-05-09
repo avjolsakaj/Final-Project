3ab369b86da30c55e8803a5c76a495cab4f502ae
Avjol Sakaj <avjolsakaj@outlook.com>
2019-05-09 18:02:17 +0200

    Deploying into firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseAppConfig: {
+    apiKey: 'AIzaSyBEzsHxIteaQKWnmC1ESAcABqOsn3L06Zg',
+    authDomain: 'fitness-app-angular.firebaseapp.com',
+    databaseURL: 'https://fitness-app-angular.firebaseio.com',
+    projectId: 'fitness-app-angular',
+    storageBucket: 'fitness-app-angular.appspot.com',
+    messagingSenderId: '215065284835'
+  }
 };
